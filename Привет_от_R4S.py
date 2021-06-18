f11d2cbfb059042f5614af447ed68b251594131a
Стеба Анна Борисовна <steba.a@tps.local>
2021-06-18 20:50:04 +0300

    Initial commit

---
+++ Привет_от_R4S.py
@@ -0,0 +1,20 @@
+greeting = "Hello!"
+print(greeting)
+name_1 = 'Anna Steba'
+print(name_1)
+gender = 'female'
+print(gender)
+age = "26"
+print(age)
+weight_1 = 64
+print(weight_1)
+height_1= 1.69
+print(height_1)
+bmi = weight_1 / height_1**2
+print(bmi)
+point = "."
+comma = ","
+
+finaly_text = greeting + " " + 'My name is' +" "+ name_1 + point +" "+ 'I\'am'+ " " + age + " " + 'years old' + point
+print(finaly_text)
+
