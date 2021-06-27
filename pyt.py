0536885768cdf7c48f9db2cc545bab127eff50e7
steba.a <steba.a@readyforsky.com>
2021-06-27 13:04:22 +0300

    123

---
+++ pyt.py
@@ -0,0 +1,16 @@
+greeting = "Hello Python!"
+greeting_length = len(greeting)
+print(len(greeting))
+print(greeting[3])
+
+letter_l = greeting [3]
+print(letter_l)
+letter = "l"
+print(letter.upper())
+print(greeting[0:2])
+print(greeting[-13:-11])
+
+opening_pyth = greeting[6]
+ending_pyth = greeting[-5:-3]
+new_name_pyth = opening_pyth + 'a' + ending_pyth
+print (new_name_pyth)
